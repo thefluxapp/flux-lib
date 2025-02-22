3b6c764cfd54a5e9bc8febd63b8a8e408975ba70
Andrei Brizhinev <bragovo@gmail.com>
2025-02-22 17:42:08 +0300

    Add NATS

--- src/settings.rs
+++ src/settings.rs
@@ -9,3 +9,9 @@ pub struct HttpSettings {
 pub struct DBSettings {
     pub endpoint: String,
 }
+
+#[derive(Deserialize, Clone)]
+pub struct NATSSettings {
+    pub endpoint: String,
+    pub stream: String,
+}
