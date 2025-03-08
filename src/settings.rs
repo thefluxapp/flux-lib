25a1de12d6552a5f23ee2337f4750d7ec6cc5ed2
Andrei Brizhinev <bragovo@gmail.com>
2025-03-08 19:27:46 +0300

    Add settings

--- src/settings.rs
+++ src/settings.rs
@@ -1,4 +1,9 @@
-use serde::Deserialize;
+use std::env;
+
+use config::{Config, Environment, File};
+use serde::{de::DeserializeOwned, Deserialize};
+
+use crate::error::Error;
 
 #[derive(Deserialize, Clone)]
 pub struct HttpSettings {
@@ -14,4 +19,22 @@ pub struct DBSettings {
 pub struct NATSSettings {
     pub endpoint: String,
     pub stream: String,
+}
+
+pub trait Settings<T: DeserializeOwned> {
+    fn new() -> Result<T, Error> {
+        let app_dir = if let Ok(dir) = env::var("APP_DIR") {
+            dir
+        } else {
+            ".".into()
+        };
+
+        let config = Config::builder()
+            .add_source(File::with_name(&format!("{}/settings/default", app_dir)))
+            .add_source(File::with_name(&format!("{}/settings/local", app_dir)).required(false))
+            .add_source(Environment::with_prefix("app").separator("_"))
+            .build()?;
+
+        Ok(config.try_deserialize()?)
+    }
 }
